8d20b5683c896805d2118d9903e3f882697b7c75
kevinlau2102 <kevinlaurence49@gmail.com>
2022-10-03 20:52:31 +0700

    Update template home page and login page
    Update template home page and login page using bootstrap

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Sidebar 04</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+  <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+  <?php
+  $menu = filter_input(type: INPUT_GET, var_name: 'ahref');
+
+  switch ($menu) {
+    case 'login':
+      include_once 'view/login-view.php';
+      break;
+    case 'home':
+      include_once 'view/home-view.php';
+      break;
+    case 'about':
+      include_once 'view/about-view.php';
+      break;
+    default:
+      include_once 'view/home-view.php';
+  }
+  ?>
+</body>
+
+</html>
---
+++ view/login-view.php
@@ -0,0 +1,22 @@
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<div class="container">
+  <h1>Login</h1>
+  <form>
+    <!-- Email input -->
+    <div class="form-outline mb-4 ">
+      <label class="form-label" for="form2Example1">Username</label>
+      <input type="email" id="form2Example1" class="form-control" />
+    </div>
+
+    <!-- Password input -->
+    <div class="form-outline mb-4">
+      <label class="form-label" for="form2Example2">Password</label>
+      <input type="password" id="form2Example2" class="form-control" />
+    </div>
+
+
+    <!-- Submit button -->
+    <button type="button" class="btn btn-primary btn-block mb-4">Sign in</button>
+  </form>
+</div>
