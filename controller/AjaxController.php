02ec96930b3ab9ff6433a6816fe0aa5bdcf86a7c
kevinlau2102 <kevinlaurence49@gmail.com>
2022-12-06 02:33:24 +0700

    Create berita acara view for admin
    - Create berita acara view for admin with AJAX

---
+++ controller/AjaxController.php
@@ -0,0 +1,115 @@
+<?php
+
+class AjaxController
+{
+
+    private BeritaAcaraDao $beritaAcaraDao;
+    private JadwalDao $jadwalDao;
+    private MataKuliahDao $mataKuliahDao;
+    private RuanganDao $ruanganDao;
+    private SemesterDao $semesterDao;
+    private ProgramStudiDao $programStudiDao;
+    private UserDao $userDao;
+    private AsistenDao $asistenDao;
+
+
+    public function __construct()
+    {
+        $this->beritaAcaraDao = new BeritaAcaraDao();
+        $this->jadwalDao = new JadwalDao();
+        $this->mataKuliahDao = new MataKuliahDao();
+        $this->ruanganDao = new RuanganDao();
+        $this->semesterDao = new SemesterDao();
+        $this->programStudiDao = new ProgramStudiDao();
+        $this->userDao = new UserDao();
+        $this->asistenDao = new AsistenDao();
+    }
+
+    public function fetchJadwal($idDosen)
+    {
+
+        if (isset($idDosen) && $idDosen != '') {
+            $jadwal = $this->jadwalDao->read($idDosen);
+            echo json_encode($jadwal);
+        }
+    }
+
+    public function fetchBeritaAcara($jadwal, $dosen, $idSemester)
+    {
+
+
+        if (isset($jadwal) && $jadwal != '') {
+            $jadwalSelect = explode('-', $jadwal);
+            $kodeMatkul = $jadwalSelect[0];
+            $kelas = $jadwalSelect[1];
+            $tipeKelas = $jadwalSelect[2];
+
+            $newJadwal = new Jadwal();
+
+            $mataKuliah = new MataKuliah();
+            $mataKuliah->setIdMataKuliah($kodeMatkul);
+
+            $newJadwal->setMataKuliah($mataKuliah);
+
+            $user = new User();
+            $user->setIdUser($dosen);
+
+            $newJadwal->setUser($user);
+
+            $semester = new Semester();
+            $semester->setIdSemester($idSemester);
+            $newJadwal->setSemester($semester);
+
+            $newJadwal->setKelas($kelas);
+            $newJadwal->setTipeKelas($tipeKelas);
+            $beritaAcara = $this->beritaAcaraDao->readByUserJadwal($newJadwal);
+            echo json_encode($beritaAcara);
+        }
+    }
+}
+
+if (isset($_POST['method']) && $_POST['method'] == "fetchJadwalDosen") {
+    include_once '../utility/Connection.php';
+    include_once '../dao/AsistenDao.php';
+    include_once '../dao/UserDao.php';
+    include_once '../dao/JadwalDao.php';
+    include_once '../dao/BeritaAcaraDao.php';
+    include_once '../dao/MataKuliahDao.php';
+    include_once '../dao/RuanganDao.php';
+    include_once '../dao/SemesterDao.php';
+    include_once '../dao/ProgramStudiDao.php';
+    include_once '../entity/Asisten.php';
+    include_once '../entity/BeritaAcara.php';
+    include_once '../entity/Jadwal.php';
+    include_once '../entity/MataKuliah.php';
+    include_once '../entity/ProgramStudi.php';
+    include_once '../entity/Role.php';
+    include_once '../entity/Ruangan.php';
+    include_once '../entity/Semester.php';
+    include_once '../entity/User.php';
+    $test = new AjaxController();
+    $test->fetchJadwal($_POST['id']);
+}
+
+if (isset($_POST['method']) && $_POST['method'] == "fetchBeritaAcara") {
+    include_once '../utility/Connection.php';
+    include_once '../dao/AsistenDao.php';
+    include_once '../dao/UserDao.php';
+    include_once '../dao/JadwalDao.php';
+    include_once '../dao/BeritaAcaraDao.php';
+    include_once '../dao/MataKuliahDao.php';
+    include_once '../dao/RuanganDao.php';
+    include_once '../dao/SemesterDao.php';
+    include_once '../dao/ProgramStudiDao.php';
+    include_once '../entity/Asisten.php';
+    include_once '../entity/BeritaAcara.php';
+    include_once '../entity/Jadwal.php';
+    include_once '../entity/MataKuliah.php';
+    include_once '../entity/ProgramStudi.php';
+    include_once '../entity/Role.php';
+    include_once '../entity/Ruangan.php';
+    include_once '../entity/Semester.php';
+    include_once '../entity/User.php';
+    $test = new AjaxController();
+    $test->fetchBeritaAcara($_POST['id'], $_POST['dosen'], $_POST['semester']);
+}
--- view/staff/berita-acara-view.php
+++ view/staff/berita-acara-view.php
@@ -1,9 +1,139 @@
 <div class="wrapper">
-    
+
     <?php include_once 'view/template/sidebar.php' ?>
 
     <div class="container">
+        <h2>Berita Acara</h2>
 
-        
+        <div class="mb-3">
+            <label for="" class="form-label">Nama Dosen</label>
+            <select class="form-select" name="dosen" id="optDosen" required>
+                <?php foreach ($dataDosen as $dosen) { ?>
+                    <option value="<?= $dosen->getIdUser() ?>"><?= $dosen ?></option>
+                <?php } ?>
+            </select>
+        </div>
+
+        <div class="mb-3">
+            <label for="" class="form-label">Jadwal</label>
+            <select class="form-select" name="jadwal" id="optJadwal" required>
+                
+            </select>
+        </div>
+
+
+        <table id="dataTable" class="table table-striped dataTable" style="width:100%">
+            <thead>
+                <tr>
+                    <th>Pertemuan Ke</th>
+                    <th>Waktu Mulai</th>
+                    <th>Waktu Selesai</th>
+                    <th>Pembahasan Materi</th>
+                    <th>Catatan</th>
+                    <th>Jumlah Mahasiswa</th>
+                    <th>Foto Presensi</th>
+                    <th>Waktu Submit</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                foreach ($dataBeritaAcara as $index => $item) {
+                    echo "<tr>";
+                    echo "<td>" . $item->getPertemuan() . "</td>";
+                    echo "<td>" . date('h:i', strtotime($item->getWaktuMulai())) . "</td>";
+                    echo "<td>" . date('h:i', strtotime($item->getWaktuSelesai())) . "</td>";
+                    echo "<td>" . $item->getPembahasanMateri() . "</td>";
+                    echo "<td>" . $item->getRangkuman() . "</td>";
+                    echo "<td>" . ($item->getJumlahMahasiswa() ? $item->getJumlahMahasiswa() : 0) . "</td>";
+                    if (substr($item->getFotoPresensi(), -3) == 'pdf') {
+                        echo "<td><form method='post' action='view/pdf-view.php' target='_blank'>";
+                        echo "<input type='hidden' value='" . $item->getFotoPresensi() . "' name='url'>";
+                        echo "<button class='btn btn-success' type='submit'>Show</button>";
+                        echo "</form></td>";
+                    } else {
+                        echo "<td><img width='100px' src='" . $item->getFotoPresensi() . "'></td>";
+                    }
+                    echo "<td>" . $item->getTglBeritaAcara() . "</td>";
+                    echo "</tr>";
+                }
+                ?>
+
+            </tbody>
+        </table>
+
     </div>
 </div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script>
+    var selectDosen = $('#optDosen :selected').val();
+    $("#optDosen").change(function() {
+        selectDosen = $('#optDosen :selected').val();
+
+        var $output = $('#optJadwal').html('');
+
+        $.ajax({
+            url: 'controller/AjaxController.php',
+            type: 'post',
+            data: {
+                method: "fetchJadwalDosen",
+                id: selectDosen
+            },
+            success: function(responsedata) {
+                var response = $.parseJSON(responsedata);
+                for (var i in response) {
+                    $output.append("<option value='" + response[i].mataKuliah.idMataKuliah + "-" + response[i].kelas + "-" + response[i].tipe_kelas + "'>" + response[i].mataKuliah.idMataKuliah + " - " + response[i].mataKuliah.nama + " - " + response[i].kelas + " - " + response[i].tipe_kelas + "</option>");
+                }
+            }
+        })
+
+    });
+
+    $("#optJadwal").change(function() {
+        var selectJadwal = $('#optJadwal :selected').val();
+
+        $.ajax({
+            url: 'controller/AjaxController.php',
+            type: 'post',
+            data: {
+                method: "fetchBeritaAcara",
+                id: selectJadwal,
+                dosen: selectDosen,
+                semester: <?= $_SESSION['semester_aktif'] ?>,
+            },
+            success: function(responsedata) {
+                var response = $.parseJSON(responsedata);
+                var table = $('#dataTable').DataTable();
+                table.clear();
+                table.destroy();
+                $('#dataTable').DataTable();
+                var $output = $('#dataTable tbody').html('');
+                for (var i in response) {
+                    var waktu_mulai = new Date(response[i].waktu_mulai);
+                    var waktu_selesai = new Date(response[i].waktu_selesai);
+
+                    var foto_presensi = response[i].foto_presensi;
+
+                    $output.append('<tr>');
+                    $output.append('<td>' + response[i].pertemuan + '</td>');
+                    $output.append('<td>' + waktu_mulai.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + '</td>');
+                    $output.append('<td>' + waktu_selesai.toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) + '</td>');
+                    $output.append('<td>' + response[i].pembahasan_materi + '</td>');
+                    $output.append('<td>' + response[i].rangkuman + '</td>');
+                    $output.append('<td>' + response[i].jumlah_mahasiswa + '</td>');
+                    if (foto_presensi != null && foto_presensi.substr(-3, 3) == "pdf") {
+                        $output.append("<td><form method='post' action='view/pdf-view.php' target='_blank'>");
+                        $output.append("<input type='hidden' value='" + response[i].foto_presensi + "' name='url'>");
+                        $output.append("<button class='btn btn-success' type='submit'>Show</button>");
+                        $output.append("</form></td>");
+                    } else {
+                        $output.append("<td><img width='100px' src='" + response[i].foto_presensi + "'></td>");
+                    }
+                    $output.append('<td>' + response[i].tgl_berita_acara);
+                    $output.append('</tr>');
+                }
+            }
+        })
+
+    });
+</script>
