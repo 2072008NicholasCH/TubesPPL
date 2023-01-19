d45f93a6baadea2e9b77e99b254f0fc1abc02129
kevinlau2102 <kevinlaurence49@gmail.com>
2023-01-19 11:42:59 +0700

    Update berita acara

--- controller/DosenController.php
+++ controller/DosenController.php
@@ -19,14 +19,130 @@ class DosenController
     {
         $dataSemester = $this->semesterDao->read();
         $filterSemester = filter_input(INPUT_POST, 'filter-semester');
+        $btnUpdate = filter_input(INPUT_POST, 'btnUpdate');
         if (isset($filterSemester)) {
             $selectedSemester = $filterSemester;
         } else {
             $selectedSemester = $_SESSION['semester_aktif'];
         }
+
+        if (isset($btnUpdate)) {
+
+            $pertemuan = trim(filter_input(INPUT_POST, 'pertemuan'));
+            $jumlah_mahasiswa = trim(filter_input(INPUT_POST, 'jumlah-mahasiswa'));
+            $tanggal = trim(filter_input(INPUT_POST, 'tanggal'));
+            $waktu_mulai = trim(filter_input(INPUT_POST, 'waktu-mulai'));
+            $waktu_selesai = trim(filter_input(INPUT_POST, 'waktu-selesai'));
+            $rangkuman = trim(filter_input(INPUT_POST, 'rangkuman'));
+            $pembahasan_materi = trim(filter_input(INPUT_POST, 'pembahasan-materi'));
+
+            if (empty($pertemuan) || empty($jumlah_mahasiswa) || empty($tanggal) || empty($waktu_mulai) || empty($waktu_selesai) || empty($rangkuman) || empty($pembahasan_materi)) {
+                $message = '<i class="fa-solid fa-circle-exclamation"></i> Please fill the field properly';
+                echo "<script> bootoast.toast({
+                    message: '" . $message . "',
+                    type: 'warning',
+                    position: 'rightTop'
+                }); </script>";
+            } else {
+
+                $newBeritaAcara = new BeritaAcara();
+                $newBeritaAcara->setIdBeritaAcara(filter_input(INPUT_POST, 'pertemuan'));
+
+                $tanggal = filter_input(INPUT_POST, 'tanggal');
+                $waktu_mulai = $tanggal . ' ' . filter_input(INPUT_POST, 'waktu-mulai');
+                $waktu_selesai = $tanggal . ' ' . filter_input(INPUT_POST, 'waktu-selesai');
+                $newBeritaAcara->setWaktuMulai(date('Y/m/d h:i:s', strtotime($waktu_mulai)));
+                $newBeritaAcara->setWaktuSelesai(date('Y/m/d h:i:s', strtotime($waktu_selesai)));
+                $newBeritaAcara->setRangkuman(filter_input(INPUT_POST, 'rangkuman'));
+                $newBeritaAcara->setPembahasanMateri(filter_input(INPUT_POST, 'pembahasan-materi'));
+                $newBeritaAcara->setPertemuan(filter_input(INPUT_POST, 'pertemuan'));
+                $newBeritaAcara->setJumlahMahasiswa(filter_input(INPUT_POST, 'jumlah-mahasiswa'));
+
+                $newBeritaAcara->setIsAsisten(filter_input(INPUT_POST, 'isAsisten') ? 1 : 0);
+                $newBeritaAcara->setTglBeritaAcara(date("Y/m/d h:i:s a"));
+
+                $jadwalSelect = explode('-', filter_input(INPUT_POST, 'jadwal'));
+                $kodeMatkul = $jadwalSelect[0];
+                $kelas = $jadwalSelect[1];
+                $tipeKelas = $jadwalSelect[2];
+
+                $mataKuliah = new MataKuliah();
+                $mataKuliah->setIdMataKuliah($kodeMatkul);
+                $newBeritaAcara->setMataKuliah($mataKuliah);
+
+                if (isset($_FILES['foto-presensi']['name']) && $_FILES['foto-presensi']['name'] != '') {
+                    $directory = 'uploads/';
+                    $extension = pathinfo($_FILES['foto-presensi']['name'], PATHINFO_EXTENSION);
+                    $new_name = $_SESSION['user']->getIdUser() . '-' . $kodeMatkul . '-' . $kelas . '-' . $tipeKelas . '-' . filter_input(INPUT_POST, 'pertemuan') . '.' . $extension;
+                    $foto_pertemuan = $directory . $new_name;
+                    if ($_FILES['foto-presensi']['size'] > 1024 * 25600) {
+                        $message = '<i class="fa-solid fa-circle-exclamation"></i> File size is too large';
+                        echo "<script> bootoast.toast({
+                            message: '" . $message . "',
+                            type: 'warning',
+                            position: 'rightTop'
+                        }); </script>";
+                    } else {
+                        var_dump(move_uploaded_file($_FILES['foto-presensi']['tmp_name'], $foto_pertemuan));
+                    }
+
+                    $newBeritaAcara->setFotoPresensi($foto_pertemuan);
+                }
+
+                $newBeritaAcara->setUser($_SESSION['user']);
+
+                $jadwal = new Jadwal();
+                $jadwal->setMataKuliah($mataKuliah);
+                $jadwal->setUser($_SESSION['user']);
+                $jadwal->setKelas($kelas);
+                $jadwal->setTipeKelas($tipeKelas);
+
+                $semester = new Semester();
+                $semester->setIdSemester($_SESSION['semester_aktif']);
+                $jadwal->setSemester($semester);
+                $newBeritaAcara->setJadwal($jadwal);
+
+                $asistenDosen = new Asisten();
+                $asistenDosen->setidAsistenDosen(filter_input(INPUT_POST, 'asisten'));
+
+                $asistenDosen2 = new Asisten();
+                $asistenDosen2->setidAsistenDosen(filter_input(INPUT_POST, 'asisten2'));
+
+                $asistenDosen3 = new Asisten();
+                $asistenDosen3->setidAsistenDosen(filter_input(INPUT_POST, 'asisten3'));
+
+
+                if ($this->beritaAcaraDao->create($newBeritaAcara)) {
+                    if (filter_input(INPUT_POST, 'isAsisten')) {
+                        $this->asistenDao->assignAsisten($asistenDosen, $jadwal, filter_input(INPUT_POST, 'lama-asistensi'), filter_input(INPUT_POST, 'pertemuan'));
+                    }
+                    if (filter_input(INPUT_POST, 'isAsisten2')) {
+                        $this->asistenDao->assignAsisten($asistenDosen2, $jadwal, filter_input(INPUT_POST, 'lama-asistensi2'), filter_input(INPUT_POST, 'pertemuan'));
+                    }
+                    if (filter_input(INPUT_POST, 'isAsisten3')) {
+                        $this->asistenDao->assignAsisten($asistenDosen3, $jadwal, filter_input(INPUT_POST, 'lama-asistensi3'), filter_input(INPUT_POST, 'pertemuan'));
+                    }
+                    header('Location: index.php?ahref=dosen');
+                } else {
+                    $message = '<i class="fa-solid fa-circle-xmark"></i> Error on add berita acara';
+                    echo "<script> bootoast.toast({
+                            message: '" . $message . "',
+                            type: 'danger',
+                            position: 'rightTop'
+                        }); </script>";
+                }
+            }
+        }
+
+
         $jadwalDosen = $this->jadwalDao->read($_SESSION['user']->getIdUser(),  $selectedSemester);
+        $jadwalDosenAll = $this->jadwalDao->readAll($_SESSION['user']->getIdUser());
+
         foreach ($jadwalDosen as $jadwal) {
             $jadwal->array_berita_acara = $this->beritaAcaraDao->readByUserJadwal($jadwal);
+            // foreach($jadwal->array_berita_acara as $beritaAcara) {
+            //     $beritaAcara->array_asisten = $this->asistenDao->getAsistenBeritaAcara($beritaAcara);
+            // }
         }
         include_once 'view/dosen-view.php';
     }
--- view/dosen-view.php
+++ view/dosen-view.php
@@ -23,7 +23,7 @@
                     <select class="form-select" name="filter-semester" id="optSemester">
                         <option selected disabled>Select Semester</option>
                         <?php foreach ($dataSemester as $semester) { ?>
-                            <option value="<?= $semester->getIdSemester() ?>" <?= $selectedSemester == $semester->getIdSemester() ? 'selected' : '' ?> ><?= $semester->getNama() ?></option>
+                            <option value="<?= $semester->getIdSemester() ?>" <?= $selectedSemester == $semester->getIdSemester() ? 'selected' : '' ?>><?= $semester->getNama() ?></option>
                         <?php } ?>
                     </select>
                 </div>
@@ -70,9 +70,9 @@
 
 
 
-<?php foreach ($jadwalDosen as $index => $jadwal) { 
+<?php foreach ($jadwalDosen as $index => $jadwal) {
 
-    ?>
+?>
     <div class="modal fade" id="jadwal-<?= $index ?>" tabindex="-1" aria-hidden="true">
         <div class="modal-dialog modal-xl modal-dialog-scrollable">
             <div class="modal-content">
@@ -81,14 +81,14 @@
                     <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
-                    <?php 
+                    <?php
                     $tipeKelas = explode("-", $jadwal)[3];
                     if (trim($tipeKelas) == "Teori") {
                         echo "Total pertemuan: " . count($jadwal->array_berita_acara) . " / 16";
                     } else {
                         echo "Total pertemuan: " . count($jadwal->array_berita_acara) . " / 14";
                     }
-                    
+
                     ?>
                     <table id="example2" class="table table-bordered table-striped dataTable" style="width:100%">
                         <thead>
@@ -101,17 +101,17 @@
                                 <th>Jumlah Mahasiswa</th>
                                 <th>Foto Presensi</th>
                                 <th>Waktu Submit</th>
+                                <th>Action</th>
                             </tr>
                         </thead>
                         <tbody>
-                            
                             <?php
                             foreach ($jadwal->array_berita_acara as $index => $item) {
 
                                 echo "<tr>";
                                 echo "<td>" . $item->getPertemuan() . "</td>";
-                                echo "<td>" . date('h:i', strtotime($item->getWaktuMulai())) . "</td>";
-                                echo "<td>" . date('h:i', strtotime($item->getWaktuSelesai())) . "</td>";
+                                echo "<td>" . date('h:i a', strtotime($item->getWaktuMulai())) . "</td>";
+                                echo "<td>" . date('h:i a', strtotime($item->getWaktuSelesai())) . "</td>";
                                 echo "<td>" . $item->getPembahasanMateri() . "</td>";
                                 echo "<td>" . $item->getRangkuman() . "</td>";
                                 echo "<td>" . ($item->getJumlahMahasiswa() ? $item->getJumlahMahasiswa() : 0) . "</td>";
@@ -124,6 +124,8 @@
                                     echo "<td><img width='100px' src='" . $item->getFotoPresensi() . "'></td>";
                                 }
                                 echo "<td>" . $item->getTglBeritaAcara() . "</td>";
+                                echo "<td><button class='btn btn-warning' data-dismiss='modal' data-toggle='modal' data-target='#beritaAcara-" . $item->getIdBeritaAcara() . "'><i class='fa-solid fa-pen-to-square'></i></button></td>";
+
                                 echo "</tr>";
                             }
                             ?>
@@ -132,6 +134,160 @@
                     </table>
                 </div>
                 <div class="modal-footer">
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+
+<?php foreach ($jadwal->array_berita_acara as $index => $item) { ?>
+
+    <div class="modal fade" id="beritaAcara-<?= $item->getIdBeritaAcara() ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-xl" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5">Edit berita acara</h1>
+                    <button type="button" class="btn-close" data-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <form id="form" method="post" enctype="multipart/form-data">
+
+                        <div class="form-group">
+                            <label for="" class="form-label">Jadwal</label>
+                            <select class="form-control custom-select" name="jadwal" required>
+                                <?php foreach ($jadwalDosenAll as $jadwal) { ?>
+                                    <option value="<?= $jadwal->getMataKuliah()->getIdMataKuliah() . '-' . $jadwal->getKelas() . '-' . $jadwal->getTipeKelas() ?>"><?= $jadwal ?></option>
+                                <?php } ?>
+                            </select>
+                        </div>
+
+
+                        <div class="form-group col-2">
+                            <label for="" class="form-label">Pertemuan</label>
+                            <input type="number" min="1" max="16" class="form-control" id="" name="pertemuan" value="<?= $item->getPertemuan() ?>" required>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Tanggal</label>
+                            <input type="date" class="form-control" id="" value="<?php 
+
+                            echo date_format(date_create($item->getWaktuMulai()), 'Y-m-d'); ?>" name="tanggal" required>
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Waktu Mulai</label>
+                            <input type="time" class="form-control" id="" value="<?php
+                            echo date('h:i', strtotime($item->getWaktuMulai())); ?>" name="waktu-mulai" required>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Waktu Selesai</label>
+                            <input type="time" class="form-control" id="" value="<?php echo date('h:i', strtotime($item->getWaktuSelesai())); ?>" name="waktu-selesai" required>
+                        </div>
+
+                        <div class="form-group col-2">
+                            <label for="" class="form-label">Jumlah Mahasiswa</label>
+                            <input type="number" min="1" class="form-control" id="" name="jumlah-mahasiswa" value="<?= $item->getJumlahMahasiswa() ?>" required>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Rangkuman Materi</label>
+                            <textarea class="form-control" id="" rows="10" name="pembahasan-materi" required><?= $item->getPembahasanMateri() ?></textarea>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Catatan</label>
+                            <textarea class="form-control" id="" rows="3" name="rangkuman" required><?= $item->getRangkuman() ?></textarea>
+
+                        </div>
+
+                        <h5 class="my-4">Asistensi</h5>
+
+                        <div class="form-check mb-3">
+                            <input class="form-check-input" type="checkbox" value="true" id="isAsisten" name="isAsisten">
+                            <label class="form-check-label" for="isAsisten">
+                                Dibantu Asisten
+                            </label>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="form-label">Asisten</label>
+                            <select class="form-control custom-select" aria-label="Default select example" id="select-asisten" disabled name="asisten">
+
+                            </select>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Lama Asistensi</label>
+                            <input type="number" min="0" class="form-control" disabled id="lama-asisten" name="lama-asistensi">
+
+                        </div>
+
+                        <div class="form-check mb-2">
+                            <input class="form-check-input" type="checkbox" value="true" id="isAsisten2" name="isAsisten2">
+                            <label class="form-check-label" for="isAsisten2">
+                                Dibantu Asisten 2
+                            </label>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="form-label">Asisten 2</label>
+                            <select class="form-select" aria-label="Default select example" id="select-asisten2" disabled name="asisten2">
+                                <option value="" disabled selected>Select your option</option>
+
+                            </select>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Lama Asistensi</label>
+                            <input type="number" min="0" class="form-control" disabled id="lama-asisten2" name="lama-asistensi2">
+
+                        </div>
+
+                        <div class="form-check mb-2">
+                            <input class="form-check-input" type="checkbox" value="true" id="isAsisten3" name="isAsisten3">
+                            <label class="form-check-label" for="isAsisten3">
+                                Dibantu Asisten 3
+                            </label>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="" class="form-label">Asisten 3</label>
+                            <select class="form-select" aria-label="Default select example" id="select-asisten3" disabled name="asisten3">
+                                <option value="" disabled selected>Select your option</option>
+
+                            </select>
+
+                        </div>
+
+                        <div class="form-group col-3">
+                            <label for="" class="form-label">Lama Asistensi</label>
+                            <input type="number" min="0" class="form-control" disabled id="lama-asisten3" name="lama-asistensi3">
+
+                        </div>
+
+                        <div class="form-group">
+                            <label for="formFile" class="form-label">Upload Bukti Foto</label>
+                            <p>Jika tidak ada file yang dimasukkan, file sebelumnya yang akan digunakan.</p>
+                            <input class="form-control" type="file" id="foto-presensi" name="foto-presensi" accept="image/png, image/jpeg, .pdf">
+                        </div>
+
+                        <div class="form-group">
+                            <button class="btn btn-primary" type="submit" name="btnSubmit">Submit</button>
+                        </div>
+
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <input type="submit" class="btn btn-primary" value="Update Berita Acara" name="btnUpdate">
                     <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
                 </div>
             </div>
